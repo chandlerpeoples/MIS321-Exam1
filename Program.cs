ba6a0c4546fb6f0fe90df96209b91e0511c58ad0
chandlerpeoples <cbpeoples1@crimson.ua.edu>
2022-02-21 14:57:04 -0600

    setting up before test

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MIS321_Exam1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
